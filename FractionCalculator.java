9fb99e224672c8c6d720566c2983e2636909b411
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-11-22 13:35:22 +0000

    clean up test code

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -2,9 +2,6 @@ package cw2;
 
 public class FractionCalculator {
 	
-	public void evaluate (Fraction fr, String input) {
-		
-		input.split(" ");
 		
 	}
-}
+
--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -2,16 +2,7 @@ package cw2;
 
 public class FractionCalculatorTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String yahh = "Hello World";
-		String[] parts = yahh.split(" ");
-		System.out.println(parts.length); 
 		
-		System.out.println(parts[0]);
-		System.out.println();
 	}
 }
