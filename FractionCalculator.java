45c03916a4e86dc00b7737a4ab20b42ea2769cf7
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-11-22 17:54:48 +0000

    evaluate() changes
    added spilt to evaluate() and introduce a for-loop to loop through the
    newly created array

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -12,14 +12,12 @@ public class FractionCalculator {
 		
 	}
 	
-		private void launch() {
+	private void launch() {
 
 			Scanner sc = new Scanner (System.in);
 			System.out.println("Welcome to Andrean Georgiev's fraction calculator");
 			System.out.println("Enter the expressions. Enter q or Q to exit at any time.");
-			
-		 
-			
+						
 			while (true) {
 				String input = sc.nextLine();
 				if(input.equals("Q") | input.equals("q")) {
