2e5d4e3d1f93bb0df393104dfe8307a7ba26f930
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-11-22 19:32:01 +0000

    testing the operator
    this test inputs an operator as a string and prints it. Then "it tries"
    to add another consecutive operator which is not executed and results in
    an error

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -3,6 +3,16 @@ package cw2;
 import java.util.Scanner;
 
 public class FractionCalculatorTest {
-
 	
+	
+	public static void main (String[] args){ 
+	
+		FractionCalculator calculator = new FractionCalculator();
+		Fraction testFraction = new Fraction(1,4);
+	    calculator.evaluate(testFraction, "+");
+	    System.out.println(calculator.currentValue+ " " +calculator.operator);
+	    // the following line tries to add another consecutive operator and should print an error
+	    calculator.evaluate(testFraction, "+");
+	
+	}
 }
