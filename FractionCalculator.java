4b867edb5538090caa68b0812b57a6319de82ef0
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-11-22 13:34:01 +0000

    start of FractionCalculator and FractionCalculatorTest

---
+++ FractionCalculator.java
@@ -0,0 +1,10 @@
+package cw2;
+
+public class FractionCalculator {
+	
+	public void evaluate (Fraction fr, String input) {
+		
+		input.split(" ");
+		
+	}
+}
---
+++ FractionCalculatorTest.java
@@ -0,0 +1,17 @@
+package cw2;
+
+public class FractionCalculatorTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String yahh = "Hello World";
+		String[] parts = yahh.split(" ");
+		System.out.println(parts.length); 
+		
+		System.out.println(parts[0]);
+		System.out.println();
+	}
+}
