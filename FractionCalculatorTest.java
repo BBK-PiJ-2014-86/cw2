0a7e594b8b715ce4f0b63014141d92151c3ace48
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-11-23 12:53:42 +0000

    testing abs

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -9,18 +9,20 @@ public class FractionCalculatorTest {
 	
 		FractionCalculator calculator = new FractionCalculator();
 		Fraction testFraction = new Fraction(1,4);
-	    //calculator.evaluate(testFraction, "+");
-	    //System.out.println(calculator.currentValue+ " " +calculator.operator);
+//	    calculator.evaluate(testFraction, "+");
+//	    System.out.println(calculator.currentValue+ " " +calculator.operator);
 	    // the following line tries to add another consecutive operator and should print an error
-	    //calculator.evaluate(testFraction, "+");
-	    
-	    calculator.evaluate(testFraction, "1/4 + 2/4");
+	    /*calculator.evaluate(testFraction, "+");
+	   	    
+		calculator.evaluate(testFraction, "1/4 + 2/4");
 	    System.out.println(calculator.currentValue+ " " +calculator.operator);
 	    calculator.evaluate(testFraction, "1/4 - 2/4");
 	    System.out.println(calculator.currentValue+ " " +calculator.operator);
 	    calculator.evaluate(testFraction, "1/4 * 2/4");
 	    System.out.println(calculator.currentValue+ " " +calculator.operator);
 	    calculator.evaluate(testFraction, "1/4 / 2/4");
+        System.out.println(calculator.currentValue+ " " +calculator.operator);*/
+        calculator.evaluate(testFraction, "1/4 - 2/4 abs");
 	    System.out.println(calculator.currentValue+ " " +calculator.operator);
 	}
 }
